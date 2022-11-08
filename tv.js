c38633de43f6f8e7f15692bf3a9a46d18bc913a7
RyOr-1 <99699014+RyOr-1@users.noreply.github.com>
2022-11-08 17:45:44 -0500

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,28 @@
+var status = "";
+
+function preload() {
+  img = loadImage("tv.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  detecter = ml5.objectDetector("cocossd", modelLoaded);
+  document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function modelLoaded() {
+  status = true;
+  console.log("Model Loaded Correctly!");
+  detecter.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(results)
+}
+}
+
+function draw() {}
